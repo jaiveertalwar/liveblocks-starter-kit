1bea65a292a390598f09146b0cab757b2b1ad345
Jaiveer Talwar <72844076+jaiveertalwar@users.noreply.github.com>
2023-08-15 16:33:09 +0530

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { getUser } from "../../../lib/server";
 import { User } from "../../../types";
-import axios from "axios";
-import qs from "qs";
 
 // Your NextAuth secret (generate a new one for production)
 // More info: https://next-auth.js.org/configuration/options#secret
@@ -26,37 +26,58 @@ export const authOptions = {
   pages: {
     signIn: "/signin",
   },
+
+  // Configure one or more authentication providers
+  // More info: https://next-auth.js.org/providers/
   providers: [
-    {
-      id: "google",
-      name: "Google",
-      type: "oauth",
-      version: "2.0",
-      scope: "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email",
-      params: { grant_type: "authorization_code" },
-      accessTokenUrl: "https://oauth2.googleapis.com/token",
-      authorizationUrl:
-        `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${process.env.GOOGLE_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${process.env.NEXTAUTH_URL}/api/auth/callback/google&state=${encodeURIComponent(
-          JSON.stringify({ callbackUrl: `${process.env.NEXTAUTH_URL}/api/auth/callback/google` })
-        )}`,
-      profileUrl: "https://www.googleapis.com/oauth2/v3/userinfo",
-      async profile(profile: any, tokens: any) {
-        const response = await axios.get(profileUrl, {
-          headers: {
-            Authorization: `Bearer ${tokens.access_token}`
-          }
-        });
+    // CredentialsProvider is used for the demo auth system
+    // Replace this with a real provider, e.g. GitHub, Auth0
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "email",
+          type: "text",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials) {
+          return null;
+        }
+
+        const user: User | null = await getUser(credentials.email);
+
+        if (!user) {
+          throw new Error("User not found");
+        }
 
         return {
-          id: response.data.sub,
-          name: response.data.name,
-          email: response.data.email,
-          image: response.data.picture,
+          id: user.id,
+          name: user.name,
+          email: user.id,
+          image: user.avatar,
         };
       },
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    },
+    }),
+
+    /*
+    // Use GitHub authentication
+    // import GithubProvider from "next-auth/providers/github";
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+    */
+
+    /*
+    // Use Auth0 authentication
+    // import Auth0Provider from "next-auth/providers/auth0";
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID as string,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
+      issuer: process.env.AUTH0_ISSUER_BASE_URL,
+    }),
+    */
 
     // ...add more providers here
   ],
