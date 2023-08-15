651a625b910502ebac260eff2b6761b88d4c581f
Jaiveer Talwar <72844076+jaiveertalwar@users.noreply.github.com>
2023-08-15 16:44:41 +0530

    Update users.ts

--- data/users.ts
+++ data/users.ts
@@ -9,30 +9,31 @@ import { User } from "../types";
  * Group info is in /data/groups.ts
  */
 export const users: User[] = [
-  /*
-  {
-    id: "[YOUR EMAIL ADDRESS]",
-    name: "[YOUR DISPLAY NAME]",
-    avatar: "https://liveblocks.io/avatars/avatar-0.png",
-    groupIds: ["product", "engineering", "design"],
-  },
-  */
   {
     id: "contact@jtlr.org",
     name: "CAJCS Student",
     avatar: "https://cathedral-school.com/wp-content/uploads/2017/03/logo.png",
     groupIds: ["product", "engineering", "design"],
+    color: getRandomColor(),
   },
   {
     id: "contact+1@jtlr.org",
     name: "CAJCS Teacher",
     avatar: "https://cathedral-school.com/wp-content/uploads/2017/03/logo.png",
     groupIds: ["engineering"],
+    color: getRandomColor(),
   },
   {
     id: "jtlr@group.com",
     name: "Other",
     avatar: "https://www.jtlrgroup.com/wp-content/uploads/2022/05/jtlr.svg",
     groupIds: ["engineering", "design"],
-  },
+    color: getRandomColor(),
+  }
 ];
+
+function getRandomColor(): string {
+  const colors = ["#FF5733", "#33FF77", "#3388FF", "#FF33E8", "#33FFE7", "#FF33B8"];
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  return colors[randomIndex];
+}
